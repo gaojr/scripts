b9c5a63afdde68eb9f3dad48bb79888db7cf2765
gaojr <gaojr427@hotmail.com>
2019-11-18 17:38:51 +0800

    Update remove_empty_folders.ps1

--- remove_empty_folders.ps1
+++ remove_empty_folders.ps1
@@ -1,5 +1,12 @@
 Set-ExecutionPolicy RemoteSigned
 
-while (gci -R . | where {$_.PSISContainer -and @( $_ | gci).Count -eq 0}) {
-    gci -R . | where {$_.PSISContainer -and @($_ | gci).Count -eq 0} | ri
+foreach ($path in $args) {
+    if  (Test-Path -Path $path -PathType Container) {
+        # 路径存在且是目录
+        while (gci -R . | where {$_.PSISContainer -and @( $_ | gci).Count -eq 0}) {
+            gci -R . | where {$_.PSISContainer -and @($_ | gci).Count -eq 0} | ri
+        }
+    } else {
+        echo "'$path' is not a directory or does not exist!"
+    }
 }
