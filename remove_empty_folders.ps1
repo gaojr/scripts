ceb0c565410f79000ee45b76a83dae6507175c39
gaojr <gaojr427@hotmail.com>
2019-11-18 16:52:42 +0800

    Update remove_empty_folders.ps1

--- remove_empty_folders.ps1
+++ remove_empty_folders.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy RemoteSigned
 while (gci -R . | where {$_.PSISContainer -and @( $_ | gci).Count -eq 0}) {
     gci -R . | where {$_.PSISContainer -and @($_ | gci).Count -eq 0} | ri
 }
