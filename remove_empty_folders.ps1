2044027301ed1caeab4c70dfb70ce13386a3a5ac
gaojr <gaojr427@hotmail.com>
2020-05-23 23:25:43 +0800

    别名换为全名

--- remove_empty_folders.ps1
+++ remove_empty_folders.ps1
@@ -3,10 +3,10 @@ Set-ExecutionPolicy RemoteSigned
 foreach ($path in $args) {
     if  (Test-Path -Path $path -PathType Container) {
         # 路径存在且是目录
-        while (gci -R . | where {$_.PSISContainer -and @( $_ | gci).Count -eq 0}) {
-            gci -R . | where {$_.PSISContainer -and @($_ | gci).Count -eq 0} | ri
+        while (Get-ChildItem -R . | Where-Object {$_.PSISContainer -and @( $_ | Get-ChildItem).Count -eq 0}) {
+            Get-ChildItem -R . | Where-Object {$_.PSISContainer -and @($_ | Get-ChildItem).Count -eq 0} | Remove-Item
         }
     } else {
-        echo "'$path' is not a directory or does not exist!"
+        Write-Output "'$path' is not a directory or does not exist!"
     }
 }
