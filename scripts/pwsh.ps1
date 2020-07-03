e0d28b3913ae8b8784f5acaf3b9edba472403de8
gaojr <gaojr427@hotmail.com>
2020-07-03 16:08:08 +0800

    powershell方法: 清楚所有过时的模块

---
+++ scripts/pwsh.ps1
@@ -0,0 +1,35 @@
+function cleanOutDatedModules {
+  <#
+  .DESCRIPTION
+      clean outdated modules
+  #>
+  Write-Host "this will remove all old versions of installed modules"
+  Write-Host "be sure to run this as an admin" -ForegroundColor Yellow
+  Write-Host "(You can update all your Azure RM modules with Update-Module Azurerm -Force)"
+
+  $mods = Get-InstalledModule
+
+  foreach ($mod in $mods)
+  {
+    Write-Host "Checking $($mod.name)"
+    $latest = Get-InstalledModule $mod.name
+    $specificmods = Get-InstalledModule $mod.name --AllVersions
+    Write-Host "$($specificmods.count) versions of this module found [ $($mod.name) ]"
+
+    foreach ($sm in $specificmods)
+    {
+      if ($sm.version -ne $latest.version)
+    {
+      Write-Host "uninstalling $($sm.name) - $($sm.version) [latest is $($latest.version)]"
+      $sm | Uninstall-Module -Force
+      Write-Host "done uninstalling $($sm.name) - $($sm.version)"
+      Write-Host "    --------"
+    }
+
+    }
+    Write-Host "------------------------"
+  }
+  Write-Host "done"
+}
+
+cleanOutDatedModules;
