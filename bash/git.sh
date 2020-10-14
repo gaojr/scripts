93ef749ec4b4aa4e25f93dfbe136af2d5d5d810d
gaojr <gaojr427@hotmail.com>
2020-10-14 10:58:21 +0800

    :triangular_flag_on_post: bash: git

---
+++ bash/git.sh
@@ -0,0 +1,11 @@
+opt=$1
+
+if [[ $opt == "f" ]]; then
+  git fetch --all --tags --prune --prune-tags;
+elif [[ $opt == "s" ]]; then
+  git status --short --branch;
+elif [[ $opt == "fv" ]]; then
+  git branch -a --sort=authordate --list '*release/*' --contains $2 | head -n 1;
+else
+  git branch -vv;
+fi
