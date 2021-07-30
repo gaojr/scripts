29e5e74712dc8cea5cc3fb9819f8fd49fd769628
gaojr <gaojr427@hotmail.com>
2021-07-30 12:42:17 +0800

    :triangular_flag_on_post: bash: git

--- bash/git.sh
+++ bash/git.sh
@@ -6,6 +6,8 @@ elif [[ $opt == "s" ]]; then
   git status --short --branch;
 elif [[ $opt == "fv" ]]; then
   git branch -a --sort=authordate --list '*release/*' --contains $2 | head -n 1;
+elif [[ -n "$opt" ]]; then
+  git $*;
 else
   git branch -vv;
 fi
