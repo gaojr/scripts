f7aa8d8b221ac81afa69f914219986f29a006b83
gaojr <gaojr427@hotmail.com>
2023-02-13 17:20:25 +0800

    🚩 bash: git

--- bash/git.sh
+++ bash/git.sh
@@ -5,6 +5,8 @@ if [[ $opt == "f" ]]; then
 elif [[ $opt == "s" ]]; then
   git status --short --branch;
 elif [[ $opt == "fv" ]]; then
+  git branch -a --sort=authordate --list '*/'$2'/*' --contains $3 | sort;
+elif [[ $opt == "fv1" ]]; then
   git branch -a --sort=authordate --list '*/release/*' --contains $2 | sort | head -n 1;
 elif [[ -n "$opt" ]]; then
   git $*;
